f9196c0b4146f328765e4698766bc81b4907ab87
Ronit Panda <pandaronit25@gmail.com>
2021-11-06 21:25:48 +0530

    divided the project up and made the plugin to transpile and bundle all the codefiles in the browser no user can directly import any package from npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import classes from './App.module.css';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: './esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+    setCode(result.outputFiles[0].text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>J book protoype-1</h3>
+      <textarea
+        className={classes.ta}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button className={classes.btn} onClick={onClick}>
+          Submit
+        </button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 }
