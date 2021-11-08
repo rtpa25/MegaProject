9de040d8942df7d1cefa32339cf22a95da4744c2
Ronit Panda <pandaronit25@gmail.com>
2021-11-08 21:07:32 +0530

    added the text-editor component just need to make the global state managment

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,15 @@
 /** @format */
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import CodeCell from './code-cell';
+import './app.css';
+import TextEditor from './text-editor';
 
 function App() {
   return (
     <div>
-      <h3>J book protoype-1</h3>
-      <CodeCell />
+      <h3 className='subtitle is-1 head'>J book</h3>
       {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   );
 }
