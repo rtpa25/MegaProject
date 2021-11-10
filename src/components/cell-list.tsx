f317b4e211199eaf0202f8e498b9e9c783489b90
Ronit Panda <pandaronit25@gmail.com>
2021-11-10 12:23:09 +0530

    made styling tuchups

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -4,6 +4,7 @@ import { Fragment } from 'react';
 import { useAppSelector } from '../hooks';
 import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
+import './cell-list.css';
 
 const CellList: React.FC = () => {
   const order = useAppSelector((state) => state.cell.order);
@@ -20,7 +21,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className='cell-list'>
       <AddCell forceVisible={cells.length === 0} prevCellId={null} />
       {renderedCells}
     </div>
